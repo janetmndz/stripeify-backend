2f7df74be0846bd5352174c696210c0d37c34dd1
Janet Mendez <janetmendez.03@gmail.com>
2019-10-01 23:57:56 -0400

    fixed playlist models

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,5 +2,5 @@ class Playlist < ApplicationRecord
   belongs_to :user
   has_many :playlist_songs, dependent: :destroy
   has_many :tracks, through: :playlist_songs
-  has_many :votes
+  has_many :votes, dependent: :destroy
 end
